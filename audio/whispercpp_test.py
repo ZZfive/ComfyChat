b6647eecf07de930769d5bbd7cc949187901940a
zjy <626958258@qq.com>
2024-06-27 00:08:24 +0800

    add audio2text in demo

--- audio/whispercpp_test.py
+++ audio/whispercpp_test.py
@@ -1,3 +1,7 @@
 from whispercpp import Whisper
+from whispercpp import api
+import numpy as np
 
-w = Whisper.from_pretrained("tiny.en")
+w = Whisper.from_pretrained("base")
+a = w.transcribe(np.ones((1, 16000))[0])
+print(a)
