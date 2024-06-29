4db14b1f27636406a00606805b1ecdd8ee74bd86
zjy <626958258@qq.com>
2024-06-29 14:25:31 +0800

    add whisperx for audio2text

--- audio/whispercpp_test.py
+++ audio/whispercpp_test.py
@@ -1,7 +1,16 @@
-from whispercpp import Whisper
-from whispercpp import api
-import numpy as np
+# from whispercpp import Whisper
+# from whispercpp import api
+# import numpy as np
 
-w = Whisper.from_pretrained("base")
-a = w.transcribe(np.ones((1, 16000))[0])
-print(a)
+# w = Whisper.from_pretrained("base")
+# a = w.transcribe(np.ones((1, 16000))[0])
+# print(a)
+
+import whisperx
+
+device = "cuda"
+compute_type: str = 'float16'
+model = whisperx.load_model("large-v2", device, compute_type=compute_type, language='en',
+                            download_root='/root/code/ComfyChat/weights')  # large-v2
+
+# audio = whisperx.load_audio()
