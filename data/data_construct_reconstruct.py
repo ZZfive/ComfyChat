1f1e7712bac9e5eafd52a2df8f60432327fec8bf
zjy <626958258@qq.com>
2024-08-20 00:04:33 +0800

    Start refactoring dataset construction

---
+++ data/data_construct_reconstruct.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   demo.py
+@Time    :   2024/08/19 23:45:56
+@Author  :   zzfive 
+@Desc    :   None
+'''
+
+
+# TODO 定义一个类，将使用不同LLM接口进行翻译的所有函数封装
+class LLMTranslator:
+    pass
+
+# TODO 定义一个类，将使用不同LLM接口生成问答数据的所有函数封装
+class LLMMessagesGenerator:
+    pass
+
+# TODO 简化当前对四个开源社区的数据提炼过程
+
+
+# TODO 优化各数据块混合方案
